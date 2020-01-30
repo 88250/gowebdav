fe241089d81d0cc891659e82850e81ab852370fb
Liang Ding <d@b3log.org>
2020-01-30 22:11:14 +0800

    :art: 重命名文件支持中文路径

--- utils_test.go
+++ utils_test.go
@@ -27,6 +27,7 @@ func ExamplePathEscape() {
 	fmt.Println(PathEscape("/web"))
 	fmt.Println(PathEscape("/web/"))
 	fmt.Println(PathEscape("/w e b/d a v/s%u&c#k:s/"))
+	fmt.Println(PathEscape("/目录1/目录2/"))
 
 	// Output:
 	//
@@ -34,6 +35,7 @@ func ExamplePathEscape() {
 	// /web
 	// /web/
 	// /w%20e%20b/d%20a%20v/s%25u&c%23k:s/
+	// /%E7%9B%AE%E5%BD%951/%E7%9B%AE%E5%BD%952/
 }
 
 func TestEscapeURL(t *testing.T) {
